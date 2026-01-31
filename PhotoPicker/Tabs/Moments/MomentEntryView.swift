6553e0e9d01e50fc809036ab056361a39492aebb
Jatin Foujdar <jatin321001@gmail.com>
2026-01-31 23:45:57 +0530

    PhotosPicker commit

--- PhotoPicker/Tabs/Moments/MomentEntryView.swift
+++ PhotoPicker/Tabs/Moments/MomentEntryView.swift
@@ -6,10 +6,13 @@
 //
 
 import SwiftUI
+import PhotosUI
 
 struct MomentEntryView: View {
     @State private var text: String = ""
     @State private var note: String = ""
+    @State private var imageData: Data?
+    @State private var newImage: PhotosPickerItem?
     
     var body: some View {
         NavigationStack{
@@ -20,6 +23,26 @@ struct MomentEntryView: View {
             .navigationTitle("Grateful Moment")
         }
     }
+    
+    var photoPicker: some View {
+        PhotosPicker(selection: $newImage) {
+            Group{
+                Image(systemName: "photo.badge.plus.fill")
+                    .font(.largeTitle)
+                    .frame(height: 250)
+                    .frame(maxWidth: .infinity)
+                    .background(Color(white: 0.4, opacity: 0.32))
+            }
+                    .clipShape(RoundedRectangle(cornerRadius: 16))
+        }
+        .onChange(of: newImage){
+            guard let newImage else { return }
+            Task{
+                imageData = try await newImage.loadTransferable(type: Data.self)
+            }
+        }
+    }
+    
     
     var contentStack: some View {
         VStack(alignment: .leading){
